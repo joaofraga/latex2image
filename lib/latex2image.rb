7c44d57a8c6f89814cad4f054d28d31e9acd5f9f
Joao Fraga <jgfraga@gmail.com>
2015-11-02 14:17:10 -0200

    Added convert method

--- lib/latex2image.rb
+++ lib/latex2image.rb
@@ -4,4 +4,9 @@ require "latex2image/latex"
 require "latex2image/version"
 
 module Latex2Image
+  def self.convert(formule, output=nil)
+    latex = Latex.new(formule)
+
+    Generator.new(latex, output).generate
+  end
 end

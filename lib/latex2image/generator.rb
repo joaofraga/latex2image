9fa9c09c5e95b0aec36fbddc04e12fe759637389
Joao Fraga <jgfraga@gmail.com>
2015-11-02 14:16:43 -0200

    Finished generator class

--- lib/latex2image/generator.rb
+++ lib/latex2image/generator.rb
@@ -1,15 +1,32 @@
 module Latex2Image
   class Generator
-    def initialize(latex, format="png", output=nil)
+    attr_reader :output
+
+    def initialize(latex, output=nil)
       @latex = latex
-      @format = format if valid_format?(format)
-      @output = output || Tempfile.new(@latex.file[:name])
+      @output = File.new(output, 'w+') || Tempfile.new(%w(latex2image png))
+    end
+
+    def generate
+      generate_dvi
+      generate_png
+
+      self
     end
 
     private
 
-    def valid_format?(format)
-      Latex2Image::AVAILABLE_FORMATS.include?(format)
+    def generate_dvi
+      system "latex -output-directory=#{temp_dir} -interaction=nonstopmode #{@latex.file.path} .tex"
+      @dvi_file = "#{temp_dir}/#{File.basename(@latex.file.path, '.tex')}.dvi"
+    end
+
+    def generate_png
+      system "dvipng -q -T tight -bg transparent -D 300 #{@dvi_file} -o #{@output.path}"
+    end
+
+    def temp_dir
+      @temp_dir ||= Dir.mktmpdir
     end
   end
 end
